ded008ae21e9a8f6d5f71a3a41ed3ccc062b8554
prashat jogi <pjo.globaliasoft@gmail.com>
2022-07-13 17:42:21 +0530

    JWT Added

--- routes/auth.js
+++ routes/auth.js
@@ -32,11 +32,15 @@ router.post('/login',async(req,res)=>{
 
         OriginalPassword != req.body.password && res.status(401).json('wrong credentials');
 
-        
+        const accessToken = JWT.sign({
+            id:user._id,
+            isAdmin:user.isAdmin,
+        },process.env.JWT_SEC,{expiresIn:"3d"}
+        );
 
         const {password,...others} = user._doc;
 
-        res.status(200).json(others)
+        res.status(200).json({...others,accessToken});
 
     }catch(err){
         res.status(500).json(err)
